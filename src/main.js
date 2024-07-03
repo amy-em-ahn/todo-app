d734844ff56458289f9be328cf1f8179b6b2c128
Amy Ahn <eunmi.ahn@dcmail.ca>
2024-07-02 22:25:33 -0400

    "Resolved merge conflicts"

--- src/main.js
+++ src/main.js
@@ -1,43 +1,63 @@
-// user input a task
-// add task by clicking enter button
-// delete by clicking delete button
-// check button click, line
-// selected
-// display group: ongoing / complete
-// list all
+/*
+Name: Amy Ahn
+Version: Beta 1.0.0
+Date: 2024-07-01 ~ 02
+Feature:
+    * User can type tasks
+    * User can check tasks
+    * User can delete tasks
+    * Display a warning befor remove tasks
+    * Solting tasks
+*/
 
 let userInput = document.getElementById('user_input');
 let btnEnter = document.getElementById('btn-enter');
-btnEnter.addEventListener('click', addTask);
 let taskList = [];
+let currentFilter = 'all';
+let filteredList = [];
 
-// Access key for Enter button
-userInput.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    btnEnter.click();
-  }
-});
+// Sorting and filtering tasks
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.dropdown-menu li a');
 
-// Modal setup
-const deleteConfirmModal = new bootstrap.Modal(document.getElementById('delete-confirm-modal'));
-let taskIdToDelete = null;
+    tabs.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+            filter(event);
+            render();
+        });
+    });
 
-document.getElementById('btn-delete-confirm-modal').addEventListener('click', () => {
-  if (taskIdToDelete) {
-    taskList = taskList.filter(task => task.id !== taskIdToDelete);
-    taskIdToDelete = null;
+    // add an event to the enter button
+    btnEnter.addEventListener('click', addTask);
+
+    // Access key for Enter button
+    userInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            btnEnter.click();
+        }
+    });
+
+    // Modal setup
+    const deleteConfirmModal = new bootstrap.Modal(document.getElementById('delete-confirm-modal'));
+    let taskIdToDelete = null;
+
+    document.getElementById('btn-delete-confirm-modal').addEventListener('click', () => {
+        if (taskIdToDelete) {
+            taskList = taskList.filter(task => task.id !== taskIdToDelete);
+            taskIdToDelete = null;
+            deleteConfirmModal.hide();
+            render();
+        }
+    });
+    // Event listener to close the modal
+    document.getElementById('cancel-modal-button').addEventListener('click', () => {
     deleteConfirmModal.hide();
-    render();
-  }
+    });
 });
 
-// Event listener to close the modal
-document.getElementById('cancel-modal-button').addEventListener('click', () => {
-  deleteConfirmModal.hide();
-});
 
 function addTask() {
-
     let tasks = {
         id: randomIDGenerate(),
         taskContents: userInput.value, 
@@ -50,10 +70,20 @@ function addTask() {
 }
 
 function render() {
+    let list = [];
+
+    if (currentFilter === 'all') {
+        // display all
+        list = taskList;
+    } else {
+        // display filtered items
+        list = filteredList;
+    }
+    
     let resultHTML = "";
 
-    for (let i = 0; i < taskList.length; i++) {
-        let task = taskList[i];
+    for (let i = 0; i < list.length; i++) {
+        let task = list[i];
         resultHTML += `<div class="col-1">
                 <input class="form-check-input" type="checkbox" value="" id="checkbox" ${task.isCompleted ? 'checked' : ''} onclick="toggleComplete('${task.id}')">
             </div>
@@ -92,11 +122,37 @@ window.confirmDeleteTask = function(id) {
     deleteConfirmModal.show();
 }
 
-function editTask() {
-
-}
-
 // ID generator
 function randomIDGenerate() {
     return '_' + Math.random().toString(36).substring(2, 9);
+}
+
+// sorting function
+function filter(event) {
+    currentFilter = event.target.getAttribute('value'); // Setting the current filter correctly
+    filteredList = []; // Clearing the filteredList array
+
+    console.log("filter", currentFilter);
+    if (currentFilter === 'all') {
+        // display all items
+        render();
+    } else if (currentFilter === 'ongoing') {
+        // display ongoing items
+        for (let i = 0; i < taskList.length; i++) {
+            if (!taskList[i].isCompleted) {
+                filteredList.push(taskList[i]);
+            }
+        }
+        console.log("ongoing", filteredList);
+        render();
+    } else if (currentFilter === 'completed') {
+        // display completed items
+        for (let i = 0; i < taskList.length; i++) {
+            if (taskList[i].isCompleted) {
+                filteredList.push(taskList[i]);
+            }
+        }
+        console.log("completed", filteredList);
+        render();
+    }
 }
